e0d208148ee831914c41ef738d23c7c5a721ece4
Adeoluwa Adegoke <adeoluwaadegoke05@gmail.com>
2026-03-29 19:11:29 +0100

    added connecting to deployed backend

--- src/socket.ts
+++ src/socket.ts
@@ -1,5 +1,8 @@
 import { io, Socket } from "socket.io-client";
 
-const socket: Socket = io("http://localhost:3000", { autoConnect: false });
+const socket: Socket = io(import.meta.env.VITE_BACKEND_URL, {
+  autoConnect: false,
+  transports: ["websocket"],
+});
 
 export default socket;
